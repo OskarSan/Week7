33b8c7bbb4317b24a4d0ab55c549624fbb4a8504
OskarSan <oskar.sankila@gmail.com>
2024-12-11 11:36:26 +0200

    bitch

--- src/routes/userRoute.ts
+++ src/routes/userRoute.ts
@@ -7,63 +7,61 @@ import { User, IUser } from '../models/User'
 
 const router: Router = Router()
 
+const userList: Partial<IUser>[] = []
 
 router.post("/api/user/register", 
     body('email').escape(),
-    //.isEmail(),
     body('password').escape(),
     async (req: Request, res: Response) => {
         const errors: Result<ValidationError> = validationResult(req)
         
         if (!errors.isEmpty()) {
             console.log(errors)
-            res.status(400).json({errors: errors.array()})
+            res.status(400).json({ errors: errors.array() })
             return
         }
         
         try {
-            const existingUser: IUser | null = await User.findOne({email: req.body.email})
+            const existingUser: Partial<IUser> | undefined = userList.find(user => user.email === req.body.email)
             console.log(existingUser)
 
             if (existingUser) {
-                res.status(403).json({message: "User already exists"})
+                res.status(403).json({ message: "User already exists" })
                 return
             }
-            const salt : string = bcrypt.genSaltSync(10)
+
+            const salt: string = bcrypt.genSaltSync(10)
             const hashedPassword: string = bcrypt.hashSync(req.body.password, salt)
             
-            const user = {
+            const user: Partial<IUser> = {
                 email: req.body.email,
                 password: hashedPassword
             }
 
-            await User.create(user)
-            console.log("User created: ", user)
-            res.status(201).json(user)  
-            
-            
+            userList.push(user)
+            console.log("User added to list: ", user)
+
+            res.status(201).json(user)
+
         } catch (error) {
             console.log("Error during registration: ", error)   
-            const errorMessage = (error instanceof Error) ? error.message : 'Unknown error';
-            res.status(500).json({message: "Server error", error: errorMessage})
+            const errorMessage = (error instanceof Error) ? error.message : 'Unknown error'
+            res.status(500).json({ message: "Server error", error: errorMessage })
         }
-
-    
     }
-
-    
 )
 
 
-router.get("/api/user/list", async (req: Request, res: Response) => {
+
+router.get("/api/user/list", (req: Request, res: Response) => {
     try {
-        const users: IUser[] = await User.find().select("email password -_id")
-        res.json(users)
+        res.json(userList)
     } catch (error) {
         console.log("Error during fetching users: ", error)
-        res.status(500).json({message: "Server error"})
+        res.status(500).json({ message: "Server error" })
     }
 })
+
 
 
 
